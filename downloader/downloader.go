cffce3865bc7a46f0a59d0f57ca9a6d5ef4759b2
Joe Schafer <joe@jschaf.com>
2019-12-01 19:41:37 -0800

    Get input and gen skeleton

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -1,56 +1,50 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
 func main() {
 	client := new(http.Client)
-	sessionCookie := ""
 	year := 2019
 	if len(os.Args) < 2 {
 		log.Fatal("Need at least 1 arg, the day")
 	}
 	day, err := strconv.Atoi(os.Args[1])
 	checkError(err)
-	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
-	req, err := http.NewRequest("GET", url, nil)
-	checkError(err)
-
-	cookie := new(http.Cookie)
-	cookie.Name, cookie.Value = "session", sessionCookie
-	req.AddCookie(cookie)
-
-	resp, err := client.Do(req)
-	checkError(err)
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		log.Fatalf("Got non 200 status code: %d", resp.StatusCode)
-	}
-
 	dayPrefix := strconv.Itoa(day)
 	if day < 10 {
 		dayPrefix = fmt.Sprintf("0%d", day)
 	}
-	path := fmt.Sprintf("day%s/day%s.go", dayPrefix, dayPrefix)
+
+	inputUrl := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
+	input := getUrl(client, inputUrl)
+	inputBytes, err := ioutil.ReadAll(input)
+	checkError(err)
+	inputPath := fmt.Sprintf("day%s/input.txt", dayPrefix)
+	err = os.MkdirAll(filepath.Dir(inputPath), 0o666)
+	checkError(err)
+	inputFile, err := os.OpenFile(inputPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o666)
+	checkError(err)
+	defer inputFile.Close()
+	inputFile.Write(inputBytes)
+
+	goPath := fmt.Sprintf("day%s/day%s.go", dayPrefix, dayPrefix)
 	file, err := os.OpenFile(
-		path,
+		goPath,
 		os.O_WRONLY|os.O_CREATE|os.O_EXCL,
 		0o666)
 	checkError(err)
 	defer file.Close()
 	file.WriteString("package main\n\n")
 
-	s := bufio.NewScanner(resp.Body)
-	for s.Scan() {
-		fmt.Fprintf(file, "// %s\n", s.Text())
-	}
 	file.WriteString(`
 func main() {
 	part1()
@@ -93,6 +87,39 @@ func doPart2(n int) int {
 }
 `, dayPrefix)
 
+}
+
+func getUrl(client *http.Client, url string) io.ReadCloser {
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	cookie := createCookie()
+	req.AddCookie(cookie)
+
+	resp, err := client.Do(req)
+	checkError(err)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("Got non 200 status code: %d for url %s", resp.StatusCode, url)
+	}
+	return resp.Body
+}
+
+func createCookie() *http.Cookie {
+	path := "aoc_session.txt"
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Unable to open session value from %s", path)
+	}
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatalf("Unable to read session file from %s", path)
+	}
+	return &http.Cookie{
+		Name:  "session",
+		Value: string(bytes),
+	}
 }
 
 func checkError(err error) {
